e3f90c632b5ba9a658e64baebd241ba61dfc5dac
yukarisusaki <yukarisoprano717@gmail.com>
2022-12-21 18:51:48 +0100

    Initial commit

---
+++ src/main/java/fhtw/slm/slm_abschluss/SlmAbschlussApplication.java
@@ -0,0 +1,13 @@
+package fhtw.slm.slm_abschluss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmAbschlussApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlmAbschlussApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fhtw/slm/slm_abschluss/SlmAbschlussApplicationTests.java
@@ -0,0 +1,13 @@
+package fhtw.slm.slm_abschluss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmAbschlussApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
